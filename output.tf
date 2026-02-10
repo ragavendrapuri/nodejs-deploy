f97c565f3cc0d63fa2c0287525833ed96ddd0e8f
Raghavendra puri <124024434+ragavendrapuri@users.noreply.github.com>
2026-02-10 18:53:08 +0530

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-  description = "The public ip of the server"
-  value       = aws_instance.jenkins_host.public_ip
-}
-
-output "public_dns" {
-  description = "The public dns of the server"
-  value       = aws_instance.jenkins_host.public_dns
-}
